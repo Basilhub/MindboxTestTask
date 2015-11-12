05ca8051a4f9ee02bc21a98bce7fb902587d8fc3
Basilhub <Workbasil@mail.ru>
2015-11-12 15:47:51 +0300

    FirstCommit

---
+++ CalculatorTool/RightTriangleCalculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GeometricShapesCalculator
+{
+    /// <summary>
+    /// Класс-библиотека для методов вычисления характеристик правильного треугольника
+    /// </summary>
+    public static class RightTriangleCalculator
+    {
+        /// <summary>
+        /// Метод вычисляющий площадь прямоугольного треугольника
+        /// </summary>
+        /// <param name="a"> Катет А </param>
+        /// <param name="b"> Катет Б </param>
+        /// <returns> Вычисленная площадь </returns>
+        public static double GetArea(double a, double b)
+        {
+            try
+            {
+                // Контракт проверки данных
+                if (a <= 0 || b <= 0)
+                    throw new ArgumentException("Стороны треугольника должны быть больше нуля");
+                // Вычисление площади
+                var area = 0.5 * (a * b);
+                // Возвращение результата
+                return area;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ CalculatorToolTest/RightTriangleCalculatorTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GeometricShapesCalculator;
+
+namespace GeometricShapesCalculatorTest
+{
+    /// <summary>
+    /// Класс содержащий в себе методы тестирования
+    /// </summary>
+    [TestClass]
+    public class RightTriangleCalculatorTest
+    {
+        /// <summary>
+        /// Тест на генерацию исключения в том случае, если один из параметров - отрицательное число. 
+        /// </summary>
+        [TestMethod]
+        public void NegativeNumberParamsTest()
+        {
+            try
+            {
+                double a = 5;
+                double b = -7;
+                RightTriangleCalculator.GetArea(a, b);
+                Assert.Fail("No exception");
+            }
+            catch (Exception ex)
+            {
+                Assert.IsTrue(ex is ArgumentException);
+            }
+        }
+
+        /// <summary>
+        /// Тест для проверки нормальных значений параметров (положительные числа)
+        /// </summary>
+        [TestMethod]
+        public void RightNumberParamsTest()
+        {
+            double a = 5;
+            double b = 6;
+            double expected = 15;
+            double actual = RightTriangleCalculator.GetArea(a, b);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Тест для проверки положительных граничных значений параметров (double.MaxValue)
+        /// </summary>
+        [TestMethod]
+        public void DoubleTypeBorderParamsTest()
+        {
+            double actual = RightTriangleCalculator.GetArea(double.MaxValue, double.MaxValue);
+            Assert.IsTrue(double.IsPositiveInfinity(actual));
+        }
+    }
+}
